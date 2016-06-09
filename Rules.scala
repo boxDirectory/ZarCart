9339adb2a89ee96ef84e419fe1594c485ea68bd9
boxDirectory <zarpetkov@yahoo.com>
2016-06-09 20:58:44 +0000

    Add files via upload

---
+++ Rules.scala
@@ -0,0 +1,18 @@
+
+package com\capgemini\cart
+
+/*
+ * It would be better to implement via the Scala Rule Engine
+ * but not sure I am supposed to use it here ...
+ */
+
+class Rules {
+
+  // Offer Rules
+  
+  def buy1Get1(itemsCnt: Int, costSingle: Double): Double = 
+      ( itemsCnt/ 2 + itemsCnt% 2 )   * costSingle
+                                                  
+  def three42( itemsCnt: Int, costSingle: Double): Double = 
+      (2 * (itemsCnt/3) + (itemsCnt %3) ) * costSingle                                            
+}
---
+++ mainObject.scala
@@ -0,0 +1,24 @@
+
+package com\capgemini\cart
+
+
+/* Cart content is read from an external file, 1 line per product. 
+ * Done in the Cart class. Here is the content of the sample file:
+ * 
+ * apple
+ * apple
+ * orange
+ * apple
+ */
+
+object mainObject extends App {
+
+  println( "\nShopping Cart Pricing exercise:\n")
+
+  val register = new Pricing("C:\\Cart.txt") 
+  
+  println( "\nFor your shopping cart your bill is: " + register.priceSimple + " (no offers).\n")
+  
+  println( "For your shopping cart your bill is: "   + register.priceOffers + " (with offers).\n")
+  
+}
