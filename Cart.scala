d0cb7185daa2d6d4df782ccccc5c244331ea7b99
boxDirectory <zarpetkov@yahoo.com>
2016-06-09 21:47:43 +0000

    Add files via upload

---
+++ Cart.scala
@@ -0,0 +1,19 @@
+
+package com.capgemini.cart
+
+import scala.io.Source
+import scala.collection.mutable.MutableList
+
+class Cart( f: String) {
+  
+  var input = MutableList[String] ()
+  
+  var in = List[String]()
+ 
+  val lines = Source.fromFile(f).getLines()
+
+  def getProducts() : MutableList[ String] = {
+    for (line <- lines)  input += line.toString
+    input
+  }  
+}
---
+++ Pricing.scala
@@ -0,0 +1,31 @@
+
+package com.capgemini.cart
+
+class Pricing( filename: String) {
+  
+  val cart  = new Cart( filename)
+  
+  val rules = new Rules
+  
+  // Prices - can also be read from a file like the Cart
+  val priceMap: Map[String, Double] = Map(  ("apple", 0.60),  ("orange", 0.25) )
+  
+  val products = cart.getProducts()
+
+  def priceSimple : Double = {
+    println( "Your cart contains: " + products + "\n")
+    println( "Individual prices are " + priceMap + "\n")
+    products.map( x =>	priceMap.getOrElse(x, 0.0) ).foldLeft(0.0)(_+_)
+  }
+
+  def priceOffers : Double = {
+    val products = cart.getProducts()
+    
+    val apples  = products.count { x => x == "apple" }
+    val oranges = products.count { x => x == "orange" }
+    
+    val pr = rules.buy1Get1(apples, priceMap.getOrElse("apple", 0.0)) + rules.three42(oranges, priceMap.getOrElse("orange",0.0))  
+    pr
+  }
+
+}
